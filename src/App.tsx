789079cb453ae4279dbb3fee0abd964d706404e8
Misha Fatuev <mishafatuev@gmail.com>
2024-11-20 20:18:48 +0200

    upd 20.11.2024

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App(){
+    return(
+        <div>
+            <h1>Заголовок</h1>
+            <p>Текст</p>
+            <img src="https://cdn.fastly.steamstatic.com/steamcommunity/public/images/items/2928650/78e52b25562f1bf2fc427e6d34f62621b85acd41.gif" alt="" />
+        </div>
+    )
+}
