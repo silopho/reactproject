149fe021134a4b2b26684a13a09e97b3c1e8c45b
Misha Fatuev <mishafatuev@gmail.com>
2024-11-27 18:01:09 +0200

    upd 27.11.2024

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 interface IPost{
     title: string;
     description: string;
@@ -5,13 +7,27 @@ interface IPost{
     author: string
 }
 
+let liked = false
+
 export function Post(props: IPost){
+    const [likes, setLikes] = useState(0);
+    function incrementLikes() {
+        if (liked == false) {
+            setLikes(likes+1);
+            liked = true;
+        } else if (liked == true) {
+            setLikes(likes-1);
+            liked = false;
+        }
+    }
     return (
         <div>
             <h1>{props.title}</h1>
             <p>{props.description}</p>
             <img src={props.image} alt="" />
             <p>Автор: {props.author}</p>
+            <p>Лайки: {likes}</p>
+            <button onClick={incrementLikes}>👍</button>
         </div>
     )
 }
